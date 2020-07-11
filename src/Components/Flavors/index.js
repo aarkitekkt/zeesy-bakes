0a8c5ee62a4c4e68832c479dc4de307b1aadd11e
aarkitekkt <johnson.brycej@gmail.com>
2020-07-11 15:37:00 -0600

    add image to flavor gallery

--- src/Components/Flavors/index.js
+++ src/Components/Flavors/index.js
@@ -5,7 +5,12 @@ import flavorsList from "../../flavorsList.json"
 
 function Flavors() {
 
-    const [flavorState, setFlavorState] = useState({ ID: "default", currentFlavor: "Flavors", description: "select a flavor to learn more" });
+    const [flavorState, setFlavorState] = useState({
+        ID: "default",
+        currentFlavor: "Flavors",
+        description: "select a flavor to learn more",
+        image: "./assets/images/Flavors/flavors.png"
+    });
 
     function showFlavor(id) {
 
@@ -13,7 +18,12 @@ function Flavors() {
 
         var clickedFlavor = flavorsList.filter(flavor => flavor.ID === id);
 
-        setFlavorState({ ID: clickedFlavor[0].ID, currentFlavor: clickedFlavor[0].Flavor, description: clickedFlavor[0].Description })
+        setFlavorState({
+            ID: clickedFlavor[0].ID,
+            currentFlavor: clickedFlavor[0].Flavor,
+            description: clickedFlavor[0].Description,
+            image: clickedFlavor[0].Image
+        })
 
     }
 
@@ -23,18 +33,20 @@ function Flavors() {
                 <div id="flavorsTitle">
                     <h1>{flavorState.currentFlavor}</h1>
                     <h5>{flavorState.description}</h5>
-                    {
-                        flavorsList.map(flavor => (
-                            < Flavor
-                                flavorName={flavor.Flavor}
-                                ID={flavor.ID}
-                                onClick={() => showFlavor(flavor.ID)}
-                            />
-                        ))
-
-                    }
-
+                    <img src={flavorState.image} alt="cupcakeFlavors" />
                 </div>
+                {
+                    flavorsList.map(flavor => (
+                        < Flavor
+                            flavorName={flavor.Flavor}
+                            ID={flavor.ID}
+                            onClick={() => showFlavor(flavor.ID)}
+                        />
+                    ))
+
+                }
+
+
             </div>
         </div>
     )
