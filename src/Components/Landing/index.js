a445b34c53f4ee4a4584d16a11eb4ab96299358f
aarkitekkt <johnson.brycej@gmail.com>
2020-07-24 15:23:44 -0600

    make navbar responsive and collapsible

--- src/Components/Landing/index.js
+++ src/Components/Landing/index.js
@@ -5,7 +5,7 @@ function Landing(props) {
     return (
         <div className="landing">
             <div id="landingContent">
-                <img id="logo" src="./assets/images/logo-05.png" alt="white-logo" />
+                <img id="landingLogo" src="./assets/images/logo-05.png" alt="white-logo" />
                 <h3 id="landingTagline" className="lightText mt-5">Gourmet cakes and cupcakes for any event</h3>
             </div>
         </div>
--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,34 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 import { Link } from 'react-scroll';
 
 function Navbar(props) {
+
+    const [navState, setNavState] = useState("");
+
+    function toggleNav() {
+
+        if (navState === "open") {
+            setNavState("");
+        } else if (navState === "") {
+            setNavState("open");
+        }
+    }
+
+
     return (
-        <nav id="zeesyNav" className="navbar fixed-top text-light">
-
-            <div className="row">
-                <div className="d-flex col-12 col-md-4 justify-content-center justify-content-md-start">
-                    <Link to={props.logoBtn} smooth={true} duration={500} id="logoButton" className="btn p-0"><img id="navLogo" src="./assets/images/logo-08.png" className="p-0" alt="navbar-logo" /></Link>
-
-                </div>
-
-                <div className="d-flex col-12 col-md-8 justify-content-center justify-content-md-end">
-                    <ul className="nav pt-2">
-                        <li className="nav-item mx-1">
-                            <Link to={props.galleryBtn} smooth={true} duration={500} id="galleryButton" className="btn">gallery</Link>
-                        </li>
-                        <li className="nav-item mx-1">
-                            <Link to={props.flavorsBtn} smooth={true} duration={500} id="galleryButton" className="btn">flavors</Link>
-                        </li>
-                        <li className="nav-item mx-1">
-                            <Link to={props.pricingBtn} smooth={true} duration={500} id="galleryButton" className="btn">pricing</Link>
-                        </li>
-                        <li className="nav-item mx-1">
-                            <Link to={props.contactBtn} smooth={true} duration={500} id="galleryButton" className="btn">contact</Link>
-                        </li>
-                    </ul>
-                </div>
+        <nav id="zeesyNav">
+            <div id="navLogo">
+                <Link to={props.logoBtn} smooth={true} duration={500} id="logoButton" className="btn p-0"><img id="logo" src="./assets/images/logo-08.png" className="mt-1 ml-2" alt="navbar-logo" /></Link>
             </div>
+            <div className="hamburger" onClick={() => toggleNav()}>
+                <div className="line"></div>
+                <div className="line"></div>
+                <div className="line"></div>
+            </div>
+
+            <ul id="nav-links" className={navState}>
+                <li>
+                    <Link to={props.galleryBtn} smooth={true} duration={500} onClick={() => toggleNav()} id="galleryButton" className="btn text-white">gallery</Link>
+                </li>
+                <li>
+                    <Link to={props.flavorsBtn} smooth={true} duration={500} onClick={() => toggleNav()} id="flavorsButton" className="btn text-white">flavors</Link>
+                </li>
+                <li>
+                    <Link to={props.pricingBtn} smooth={true} duration={500} onClick={() => toggleNav()} id="pricingButton" className="btn text-white">pricing</Link>
+                </li>
+                <li>
+                    <Link to={props.contactBtn} smooth={true} duration={500} onClick={() => toggleNav()} id="contactButton" className="btn text-white">contact</Link>
+                </li>
+            </ul>
 
         </nav>
     )
