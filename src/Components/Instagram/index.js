700af3bd2893a540688ae08da34d5712ae758317
aarkitekkt <johnson.brycej@gmail.com>
2020-07-30 20:41:02 -0600

    gallery framework

--- src/Components/Instagram/index.js
+++ src/Components/Instagram/index.js
@@ -4,12 +4,49 @@ import "./style.css";
 function Instagram() {
     return (
         <div className="instagram">
-            <div id="instagramContent">
-                <div id="instagramTitle">
-                    <h1>instagram gallery</h1>
+            <h1 id="instagramTitle">instagram gallery</h1>
+            <div id="instagramContent" className="container">
+                <div className="row mx-auto">
+                    <div className="col-12 col-sm-6 col-md-4 col-lg-3 flex">
+                        <div className="gram mx-auto"></div>
+                    </div>
+                    <div className="col-12 col-sm-6 col-md-4 col-lg-3 flex">
+                        <div className="gram mx-auto"></div>
+                    </div>
+                    <div className="col-12 col-sm-6 col-md-4 col-lg-3 flex">
+                        <div className="gram mx-auto"></div>
+                    </div>
+                    <div className="col-12 col-sm-6 col-md-4 col-lg-3 flex">
+                        <div className="gram mx-auto"></div>
+                    </div>
+                    <div className="col-12 col-sm-6 col-md-4 col-lg-3 flex">
+                        <div className="gram mx-auto"></div>
+                    </div>
+                    <div className="col-12 col-sm-6 col-md-4 col-lg-3 flex">
+                        <div className="gram mx-auto"></div>
+                    </div>
+                    <div className="col-12 col-sm-6 col-md-4 col-lg-3 flex">
+                        <div className="gram mx-auto"></div>
+                    </div>
+                    <div className="col-12 col-sm-6 col-md-4 col-lg-3 flex">
+                        <div className="gram mx-auto"></div>
+                    </div>
+                    <div className="col-12 col-sm-6 col-md-4 col-lg-3 flex">
+                        <div className="gram mx-auto"></div>
+                    </div>
+                    <div className="col-12 col-sm-6 col-md-4 col-lg-3 flex">
+                        <div className="gram mx-auto"></div>
+                    </div>
+                    <div className="col-12 col-sm-6 col-md-4 col-lg-3 flex">
+                        <div className="gram mx-auto"></div>
+                    </div>
+                    <div className="col-12 col-sm-6 col-md-4 col-lg-3 flex">
+                        <div className="gram mx-auto"></div>
+                    </div>
                 </div>
             </div>
         </div>
+
     )
 }
 
