294f7ca0ad5684891e41c6b1ba13df379e057bfb
aarkitekkt <johnson.brycej@gmail.com>
2020-10-23 09:26:44 -0600

    styled and format modal

--- src/Components/Modal/index.js
+++ src/Components/Modal/index.js
@@ -8,9 +8,18 @@ function Modal() {
     return (
         <div id="modal" className={modalState}>
             <div id="modalContent">
-                <h1 id="closeBtn" onClick={() => setModalState("closed")}>×</h1>
-                <h1>Buy Some Holiday Stuff!</h1>
-                <a href="http://google.com">buy</a>
+                <h3 id="closeBtn" onClick={() => setModalState("closed")}>×</h3>
+                <div className="modalContentContainer">
+                    <img id="modalLogo" src="./assets/images/logo_gold.png" alt="ZBlogo" />
+                    <div id="modalTitle">
+                        <h2 id="christmas" className="titleText">Christmas</h2>
+                        <h2 className="titleText">Mini Boxes</h2>
+                        <h2 className="titleText">$15</h2>
+                    </div>
+                    <p className="modalText">Each box contains 16 cupcakes with up to 4 flavors and are perfect gifts for friends, neighbors, or yourself!</p>
+                    <p className="modalText">(You've earned it)</p>
+                    <a id="orderButton" href="http://google.com">Order yours now!</a>
+                </div>
             </div>
         </div >
     )
