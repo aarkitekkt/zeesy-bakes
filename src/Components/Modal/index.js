5ef8cf842e17425fe5b6e85c0cfbd8c09ee38b2f
aarkitekkt <johnson.brycej@gmail.com>
2021-01-06 20:55:55 -0700

    update modal to Valentines day content

--- src/Components/Modal/index.js
+++ src/Components/Modal/index.js
@@ -10,15 +10,16 @@ function Modal() {
             <div id="modalContent">
                 <h3 id="closeBtn" onClick={() => setModalState("closed")}>×</h3>
                 <div className="modalContentContainer">
-                    <img id="modalLogo" src="./assets/images/logo_gold.png" alt="ZBlogo" />
+                    <img id="modalLogo" src="./assets/images/logo_burgundy-03.png" alt="ZBlogo" />
                     <div id="modalTitle">
-                        <h2 id="christmas" className="titleText">Christmas</h2>
-                        <h2 className="titleText">Mini Boxes</h2>
-                        <h2 className="titleText">$15</h2>
+                        <div className="trim"></div>
+                        <h2 id="christmas" className="titleText">Valentine's Day</h2>
+                        <h2 className="titleText">Treat Boxes</h2>
+                        <div className="trim"></div>
                     </div>
-                    <p className="modalText">Each box contains 16 mini cupcakes with up to 4 flavors and are perfect gifts for friends, neighbors, or yourself!</p>
-                    <p className="modalText">(You've earned it)</p>
-                    <a id="orderButton" href="https://forms.gle/jMwsPkekkdH9ZtZ48">Order yours now!</a>
+                    <p className="modalText">Three treat box varieties with customizable flavor options.</p>
+                    <p className="modalText">Starting at $5</p>
+                    <a id="orderButton" href="https://forms.gle/b95apENFBtpegeLP6">Order yours now!</a>
                 </div>
             </div>
         </div >
